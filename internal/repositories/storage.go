42fbdc9657b25ef10ecf72e5d5a944397512fb82
MishaBah <misha.bah21@yandex.ru>
2024-07-13 17:45:49 +0300

    fix: fix

--- internal/repositories/storage.go
+++ internal/repositories/storage.go
@@ -19,7 +19,7 @@ func NewStorage(db *sql.DB) *Storage {
 
 // Save saves page to storage.
 func (s *Storage) Save(ctx context.Context, p *entity.Page) error {
-	q := `INSERT OR REPLACE INTO pages (url, user_name, name, description, category) VALUES (?, ?, ?, ?, ?);`
+	q := `INSERT INTO pages (url, user_name, name, description, category) VALUES (?, ?, ?, ?, ?)`
 
 	if _, err := s.db.ExecContext(ctx, q, p.URL, p.UserName, p.Title, p.Description, p.Category); err != nil {
 		return fmt.Errorf("can't save page: %w", err)
